346cb379ca318aae99049034ec8fab10872c1f72
Vinicius Oliveira <vinicius.shk@gmail.com>
2023-01-16 16:38:17 -0300

    TDD para requisito 7

--- tests/unit/Services/motorcycles.test.ts
+++ tests/unit/Services/motorcycles.test.ts
@@ -24,6 +24,45 @@ describe('Test suit for motorcycles', function () {
 
     expect(result).to.be.deep.equal(registerOutput);
   });
+  // it('Should list all motorcycles', async function () {
+  //   const output = [
+  //     {
+  //       id: '634852326b35b59438fbea2f',
+  //       model: 'Honda Cb 600f Hornet',
+  //       year: 2005,
+  //       color: 'Yellow',
+  //       status: true,
+  //       buyValue: 30.000,
+  //       category: 'Street',
+  //       engineCapacity: 600,
+  //     },
+  //   ];
+  //   sinon.stub(Model, 'find').resolves(output);
+
+  //   const service = new MotorcycleService();
+  //   const result = await service.findAll();
+
+  //   expect(result).to.be.deep.equal(output);
+  // });
+  // it('Should list one motorcycle by id', async function () {
+  //   const id = '634852326b35b59438fbea2f';
+  //   const output = {
+  //     id: '634852326b35b59438fbea2f',
+  //     model: 'Honda Cb 600f Hornet',
+  //     year: 2005,
+  //     color: 'Yellow',
+  //     status: true,
+  //     buyValue: 30.000,
+  //     category: 'Street',
+  //     engineCapacity: 600,
+  //   };
+  //   sinon.stub(Model, 'findOne').resolves(output);
+
+  //   const service = new MotorcycleService();
+  //   const result = await service.findById(id);
+
+  //   expect(result).to.be.deep.equal(output);
+  // });
   afterEach(function () {
     sinon.restore();
   });
